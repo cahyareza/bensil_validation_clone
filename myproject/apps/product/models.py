daa1acd21d979f955dec0e6906b70c9fc5fa06e6
cahyafaisalreza <cf.reza07@gmail.com>
2023-05-19 09:23:12 +0700

    change max length

--- myproject/apps/product/models.py
+++ myproject/apps/product/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class ProductUnique(models.Model):
     title = models.CharField(max_length=10)
-    unique = models.CharField(max_length=10, null=True, blank=True)
+    unique = models.CharField(max_length=15, null=True, blank=True)
 
     def __str__(self):
         return f"{self.title} {self.unique}"
